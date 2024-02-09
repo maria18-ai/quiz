0cf4da8249dcf3442b8b32e3a046d005eec9398e
Maria Teodoro <maduteodoro4@gmail.com>
2024-02-09 17:30:07 -0300

    Criando os arquivos de data

---
+++ src/data/questions.js
@@ -0,0 +1,35 @@
+const data = [
+    {
+        question: "Qual é o nome do arquipélago em que a ilha dos Piratas do Chapéu de Palha, Sabaody, está localizada?",
+        options: [
+        "Arquipélago de Boin",
+        "Arquipélago de Sabaody",
+        "Arquipélago de Florian",
+        "Arquipélago de Conomi",
+    ],
+        answer: "Arquipélago de Sabaody",
+    },
+    {
+        question: "Qual é o nome do irmão de Luffy que é o chefe dos revolucionários?",
+        options: ["Sabo", "Ace", "Dragon", "Garp"],
+        answer: "Sabo",
+    },
+    {
+        question: "Quem é o autor da série de mangá One Piece?",
+        options: ["Eiichiro Oda", "Masashi Kishimoto", "Akira Toriyama", "Tite Kubo"],
+        answer: "Eiichiro Oda",
+    },
+    {
+        question: "Qual é o nome do almirante da Marinha conhecido como Cão Louco?",
+        options: ["Kizaru", "Aokiji", "Akainu", "Fujitora"],
+        answer: "Akainu",
+    },
+    {
+        question:
+        "Qual é o nome do grupo de piratas liderado por Marshall D. Teach (Barba Negra) antes de se tornar um Shichibukai?",
+        options: ["Piratas do Barba Negra", "Piratas do Céu", "Piratas do Barba Branca", "Piratas do Rox"],
+        answer: "Piratas do Rox",
+    },
+];
+
+export default data;
---
+++ src/data/questions_complete.js
@@ -0,0 +1,93 @@
+const data = [
+    {
+        category: "FACEIS",
+        questions: [
+        {
+            question: "Qual é o nome do protagonista principal de One Piece?",
+            options: ["Monkey D. Luffy", " Roronoa Zoro", "Trafalgar Law", "Portgas D. Ace"],
+            answer: "Monkey D. Luffy",
+            tip: "O personagem faz parte da família dos D`s",
+        },
+        {
+            question: "Qual é o nome do chapéu que Luffy sempre usa?",
+            options: ["Chapéu de Palha", "Chapéu de Pirata", "Chapéu do Sol", " Chapéu de Aventura"],
+            answer: " Chapéu de Palha",
+            tip: "Um símbolo de amizade e promessa entre Luffy e Shanks.",
+        },
+        {
+            question: "Qual é a fruta do diabo que Luffy come, concedendo-lhe o poder de esticar seu corpo como borracha?",
+            options: ["Fruta do Diabo Hito Hito no Mi", "Fruta do Diabo Gomu Gomu no Mi", " Fruta do Diabo Mera Mera no Mi", "Fruta do Diabo Yami Yami no Mi"],
+            answer: "Fruta do Diabo Gomu Gomu no Mi",
+        },
+        {
+            question: "Quem é o capitão dos Piratas do Chapéu de Palha?",
+            options: ["Roronoa Zoro", "Nami", "Monkey D. Luffy", "Sanji"],
+            answer: "Monkey D. Luffy",
+        },
+        {
+            question: "Qual é o nome do médico dos Piratas do Chapéu de Palha?",
+            options: ["Usopp", "Tony Tony Chopper", "Nico Robin", "Franky"],
+            answer: "Tony Tony Chopper",
+        },
+    ],
+    },
+    {
+        category: "MEDIO",
+        questions: [
+        {
+            question: "Qual é o nome do arqueólogo que se juntou aos Piratas do Chapéu de Palha durante a saga de Alabasta?",
+            options: ["Nico Robin", "Franky", "Brook", "Jinbe"],
+            answer: "Nico Robin",
+            tip: "Ela tinha uma recompensa em sua cabeça antes de se juntar aos Chapéu de Palha",
+        },
+        {
+            question: "Qual é o nome do navio dos Piratas do Chapéu de Palha?",
+            options: ["Thousand Sunny", "Going Merry", "Oro Jackson", "Blackbeard"],
+            answer: "Thousand Sunny",
+        },
+        {
+            question: "Qual é o nome do país governado por um dos Yonko, Big Mom?",
+            options: ["Wano", "Dressrosa", "Whole Cake Island", "Water 7"],
+            answer: "Whole Cake Island",
+        },
+    ],
+    },
+    {
+        category: "DÍFICIL",
+        questions: [
+        {
+            question: "Qual é o nome verdadeiro do Barba Negra, um dos Yonko?",
+            options: [
+            "Marshall D. Teach",
+            "Gol D. Roger",
+            "Monkey D. Dragon",
+            "Shanks",
+        ],
+            answer: "Marshall D. Teach",
+        },
+        {
+            question: "Qual é o nome da técnica secreta de Luffy que ele aprendeu durante seu treinamento de dois anos com Rayleigh?",
+            options: ["Gear Fifth: King Kong Gun", "Red Hawk", "Elephant Gatling", "Gear Fourth: Boundman"],
+            answer: "Elephant Gatling",
+        },
+        {
+            question: "Qual é o nome da ilha onde Luffy e seus amigos encontram os Minks, uma raça animal inteligente?",
+            options: ["Zou", "Skypiea", "Fishman Island", "Drum Island"],
+            answer: "Zou",
+        },
+        {
+            question: "Quem é o indivíduo que é amplamente associado à descoberta do One Piece e é considerado o Rei dos Piratas?",
+            options: ["Gol D. Roger", "Silvers Rayleigh", "Edward Newgate (Barba Branca)", "Joy Boy"],
+            answer: "Gol D. Roger",
+        },
+        {
+            question:
+            "Qual é o nome do primeiro personagem a se juntar à tripulação dos Piratas do Chapéu de Palha depois de Luffy?",
+            options: ["Roronoa Zoro", "Nami", "Usopp", "Tony Tony Chopper"],
+            answer: "Nami",
+        },
+    ],
+    },
+];
+  
+export default data;
